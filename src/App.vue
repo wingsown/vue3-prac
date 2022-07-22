f2e24928cd5ff2a7bbf244fce19cf15cddeb076b
wjbuena <wilsonjeffrey.buena@synpulse.com>
2022-07-22 17:21:51 +0800

    Forms

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,14 @@
 <template>
-  <h1>Reaction Timer</h1>
-  <button @click="start" :disabled="isPlaying">Play</button>
-  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
-  <Results v-if="showResults" :score="score"/>
+  <SignUpForm />
 </template>
 
 <script>
-import Block from './components/Block.vue';
-import Results from './components/Results.vue'
+import SignUpForm from './components/SignUpForm.vue'
 
 export default {
   name: 'App',
-  components: { Block, Results },
-  data() {
-    return {
-      isPlaying: false,
-      delay: null,
-      score: null,
-      showResults: false
-    }
-  },
-  methods: {
-    start() {
-      this.delay = 2000 + Math.random() * 5000;
-      this.isPlaying = true;
-      this.showResults = false;
-    },
-    endGame(reactionTime) {
-      this.score = reactionTime;
-      this.isPlaying = false;
-      this.showResults = true;
-    }
-  }
+  components: { SignUpForm },
+  
   
   
 }
@@ -43,24 +20,12 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #444;
+  color: #2c3e50;
   margin-top: 60px;
 }
 
-button {
-  background: #0faf87;
-  color: #fff;
-  border: none;
-  padding: 8px 16px;
-  border-radius: 4px;
-  font-size: 16px;
-  letter-spacing: 1px;
-  cursor: pointer;
-  margin: 10px;
-}
-
-button[disabled] {
-  opacity: 0.2;
-  cursor: not-allowed;
+body {
+  margin: 0;
+  background: #eee;
 }
 </style>
